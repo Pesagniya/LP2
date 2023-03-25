c771c5aef70fd99f75430c1f9da11f0791af9a16
Guilherme U. Iwasaki <101954273+Pesagniya@users.noreply.github.com>
2023-03-24 23:57:01 -0300

    Add files via upload

---
+++ Atividade4/PTriangulo/PTriangulo/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace PTriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblA.Location = new System.Drawing.Point(225, 73);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(60, 20);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Lado A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblB.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblB.Location = new System.Drawing.Point(225, 111);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(60, 20);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Lado B";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblC.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblC.Location = new System.Drawing.Point(225, 149);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(60, 20);
+            this.lblC.TabIndex = 2;
+            this.lblC.Text = "Lado C";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(135, 244);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(139, 94);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Verificação";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(327, 244);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(139, 94);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(516, 244);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(139, 94);
+            this.btnFechar.TabIndex = 5;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(305, 73);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(201, 20);
+            this.txtA.TabIndex = 7;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(305, 111);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(201, 20);
+            this.txtB.TabIndex = 8;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(305, 149);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(201, 20);
+            this.txtC.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+    }
+}
+
---
+++ Atividade4/PTriangulo/PTriangulo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTriangulo
+{
+    public partial class Form1 : Form
+    {
+        double ladoa, ladob, ladoc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtA.Clear();
+            txtB.Clear();
+            txtC.Clear();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+
+            double.TryParse(txtA.Text, out ladoa);
+            double.TryParse(txtB.Text, out ladob);
+            double.TryParse(txtC.Text, out ladoc);
+
+            if ((ladoa < (ladob + ladoc)) && (ladoa > Math.Abs(ladob - ladoc)) ||
+               (ladob < (ladoa + ladoc)) && (ladob > Math.Abs(ladoa - ladoc)) ||
+               (ladoc < (ladoa + ladob)) && (ladoc > Math.Abs(ladoa - ladob)))
+                if (ladoa != ladob && ladoa != ladob && ladoc != ladoa)
+                    MessageBox.Show("Triângulo escaleno.");
+                else if (ladoa != ladob || ladoa != ladob || ladoc != ladoa)
+                    MessageBox.Show("Triângulo isósceles.");
+                else
+                    MessageBox.Show("Triângulo equilátero.");
+            else
+                MessageBox.Show("Não forma um triângulo.");
+        }
+    }
+}
