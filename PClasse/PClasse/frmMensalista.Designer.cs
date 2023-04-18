6fd74720d68f376a9fc8f845d4197ac14ee931b2
Guilherme U. Iwasaki <101954273+Pesagniya@users.noreply.github.com>
2023-04-18 11:12:05 -0300

    Add files via upload

---
+++ PClasse/PClasse/frmMensalista.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace PClasse
+{
+    partial class frmMensalista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMatricula = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblSalarioMes = new System.Windows.Forms.Label();
+            this.lblDataEntrada = new System.Windows.Forms.Label();
+            this.btnMensalista = new System.Windows.Forms.Button();
+            this.btnMensalista2 = new System.Windows.Forms.Button();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSalarioMes = new System.Windows.Forms.TextBox();
+            this.txtDataEntrada = new System.Windows.Forms.TextBox();
+            this.gbxHome = new System.Windows.Forms.GroupBox();
+            this.rbtnYes = new System.Windows.Forms.RadioButton();
+            this.rbtnNo = new System.Windows.Forms.RadioButton();
+            this.gbxHome.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblMatricula
+            // 
+            this.lblMatricula.AutoSize = true;
+            this.lblMatricula.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatricula.Location = new System.Drawing.Point(105, 70);
+            this.lblMatricula.Name = "lblMatricula";
+            this.lblMatricula.Size = new System.Drawing.Size(68, 18);
+            this.lblMatricula.TabIndex = 0;
+            this.lblMatricula.Text = "Matrícula";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(105, 107);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(49, 18);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblSalarioMes
+            // 
+            this.lblSalarioMes.AutoSize = true;
+            this.lblSalarioMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalarioMes.Location = new System.Drawing.Point(105, 143);
+            this.lblSalarioMes.Name = "lblSalarioMes";
+            this.lblSalarioMes.Size = new System.Drawing.Size(106, 18);
+            this.lblSalarioMes.TabIndex = 2;
+            this.lblSalarioMes.Text = "Salário Mensal";
+            // 
+            // lblDataEntrada
+            // 
+            this.lblDataEntrada.AutoSize = true;
+            this.lblDataEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataEntrada.Location = new System.Drawing.Point(105, 180);
+            this.lblDataEntrada.Name = "lblDataEntrada";
+            this.lblDataEntrada.Size = new System.Drawing.Size(198, 18);
+            this.lblDataEntrada.TabIndex = 3;
+            this.lblDataEntrada.Text = "Data de Entrada na Empresa";
+            // 
+            // btnMensalista
+            // 
+            this.btnMensalista.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMensalista.Location = new System.Drawing.Point(202, 265);
+            this.btnMensalista.Name = "btnMensalista";
+            this.btnMensalista.Size = new System.Drawing.Size(131, 100);
+            this.btnMensalista.TabIndex = 4;
+            this.btnMensalista.Text = "Instanciar Mensalista";
+            this.btnMensalista.UseVisualStyleBackColor = true;
+            this.btnMensalista.Click += new System.EventHandler(this.BtnMensalista_Click);
+            // 
+            // btnMensalista2
+            // 
+            this.btnMensalista2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMensalista2.Location = new System.Drawing.Point(408, 265);
+            this.btnMensalista2.Name = "btnMensalista2";
+            this.btnMensalista2.Size = new System.Drawing.Size(135, 100);
+            this.btnMensalista2.TabIndex = 5;
+            this.btnMensalista2.Text = "Instanciar Mensalista com parâmetros";
+            this.btnMensalista2.UseVisualStyleBackColor = true;
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(323, 71);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(82, 20);
+            this.txtMatricula.TabIndex = 6;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(323, 108);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(196, 20);
+            this.txtNome.TabIndex = 7;
+            // 
+            // txtSalarioMes
+            // 
+            this.txtSalarioMes.Location = new System.Drawing.Point(323, 144);
+            this.txtSalarioMes.Name = "txtSalarioMes";
+            this.txtSalarioMes.Size = new System.Drawing.Size(116, 20);
+            this.txtSalarioMes.TabIndex = 8;
+            // 
+            // txtDataEntrada
+            // 
+            this.txtDataEntrada.Location = new System.Drawing.Point(323, 181);
+            this.txtDataEntrada.Name = "txtDataEntrada";
+            this.txtDataEntrada.Size = new System.Drawing.Size(116, 20);
+            this.txtDataEntrada.TabIndex = 9;
+            // 
+            // gbxHome
+            // 
+            this.gbxHome.Controls.Add(this.rbtnNo);
+            this.gbxHome.Controls.Add(this.rbtnYes);
+            this.gbxHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxHome.Location = new System.Drawing.Point(551, 52);
+            this.gbxHome.Name = "gbxHome";
+            this.gbxHome.Size = new System.Drawing.Size(200, 100);
+            this.gbxHome.TabIndex = 10;
+            this.gbxHome.TabStop = false;
+            this.gbxHome.Text = "Trabalha em Home Office";
+            // 
+            // rbtnYes
+            // 
+            this.rbtnYes.AutoSize = true;
+            this.rbtnYes.Location = new System.Drawing.Point(28, 34);
+            this.rbtnYes.Name = "rbtnYes";
+            this.rbtnYes.Size = new System.Drawing.Size(52, 22);
+            this.rbtnYes.TabIndex = 0;
+            this.rbtnYes.Text = "Sim";
+            this.rbtnYes.UseVisualStyleBackColor = true;
+            // 
+            // rbtnNo
+            // 
+            this.rbtnNo.AutoSize = true;
+            this.rbtnNo.Checked = true;
+            this.rbtnNo.Location = new System.Drawing.Point(28, 62);
+            this.rbtnNo.Name = "rbtnNo";
+            this.rbtnNo.Size = new System.Drawing.Size(54, 22);
+            this.rbtnNo.TabIndex = 1;
+            this.rbtnNo.TabStop = true;
+            this.rbtnNo.Text = "Não";
+            this.rbtnNo.UseVisualStyleBackColor = true;
+            // 
+            // frmMensalista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbxHome);
+            this.Controls.Add(this.txtDataEntrada);
+            this.Controls.Add(this.txtSalarioMes);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.btnMensalista2);
+            this.Controls.Add(this.btnMensalista);
+            this.Controls.Add(this.lblDataEntrada);
+            this.Controls.Add(this.lblSalarioMes);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblMatricula);
+            this.Name = "frmMensalista";
+            this.Text = "frmMensalista";
+            this.gbxHome.ResumeLayout(false);
+            this.gbxHome.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMatricula;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblSalarioMes;
+        private System.Windows.Forms.Label lblDataEntrada;
+        private System.Windows.Forms.Button btnMensalista;
+        private System.Windows.Forms.Button btnMensalista2;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSalarioMes;
+        private System.Windows.Forms.TextBox txtDataEntrada;
+        private System.Windows.Forms.GroupBox gbxHome;
+        private System.Windows.Forms.RadioButton rbtnNo;
+        private System.Windows.Forms.RadioButton rbtnYes;
+    }
+}
