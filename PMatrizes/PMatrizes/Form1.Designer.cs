ffb60f62bea25558a6aaf80cc050fd9684286f54
Guilherme U. Iwasaki <101954273+Pesagniya@users.noreply.github.com>
2023-04-25 11:05:11 -0300

    Add files via upload

---
+++ PMatrizes/PMatrizes/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace PMatrizes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEx1 = new System.Windows.Forms.Button();
+            this.btnEx2 = new System.Windows.Forms.Button();
+            this.btnEx3 = new System.Windows.Forms.Button();
+            this.btnEx5 = new System.Windows.Forms.Button();
+            this.btnEx6 = new System.Windows.Forms.Button();
+            this.btnEx4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEx1
+            // 
+            this.btnEx1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEx1.Location = new System.Drawing.Point(183, 119);
+            this.btnEx1.Name = "btnEx1";
+            this.btnEx1.Size = new System.Drawing.Size(113, 69);
+            this.btnEx1.TabIndex = 0;
+            this.btnEx1.Text = "Exercício 1";
+            this.btnEx1.UseVisualStyleBackColor = true;
+            this.btnEx1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnEx2
+            // 
+            this.btnEx2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEx2.Location = new System.Drawing.Point(337, 119);
+            this.btnEx2.Name = "btnEx2";
+            this.btnEx2.Size = new System.Drawing.Size(113, 69);
+            this.btnEx2.TabIndex = 1;
+            this.btnEx2.Text = "Exercício 2";
+            this.btnEx2.UseVisualStyleBackColor = true;
+            this.btnEx2.Click += new System.EventHandler(this.BtnEx2_Click);
+            // 
+            // btnEx3
+            // 
+            this.btnEx3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEx3.Location = new System.Drawing.Point(490, 119);
+            this.btnEx3.Name = "btnEx3";
+            this.btnEx3.Size = new System.Drawing.Size(113, 69);
+            this.btnEx3.TabIndex = 2;
+            this.btnEx3.Text = "Exercício 3";
+            this.btnEx3.UseVisualStyleBackColor = true;
+            this.btnEx3.Click += new System.EventHandler(this.BtnEx3_Click);
+            // 
+            // btnEx5
+            // 
+            this.btnEx5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEx5.Location = new System.Drawing.Point(337, 234);
+            this.btnEx5.Name = "btnEx5";
+            this.btnEx5.Size = new System.Drawing.Size(113, 69);
+            this.btnEx5.TabIndex = 3;
+            this.btnEx5.Text = "Exercício 5";
+            this.btnEx5.UseVisualStyleBackColor = true;
+            this.btnEx5.Click += new System.EventHandler(this.BtnEx5_Click);
+            // 
+            // btnEx6
+            // 
+            this.btnEx6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEx6.Location = new System.Drawing.Point(490, 234);
+            this.btnEx6.Name = "btnEx6";
+            this.btnEx6.Size = new System.Drawing.Size(113, 69);
+            this.btnEx6.TabIndex = 4;
+            this.btnEx6.Text = "Exercício 6";
+            this.btnEx6.UseVisualStyleBackColor = true;
+            // 
+            // btnEx4
+            // 
+            this.btnEx4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEx4.Location = new System.Drawing.Point(183, 234);
+            this.btnEx4.Name = "btnEx4";
+            this.btnEx4.Size = new System.Drawing.Size(113, 69);
+            this.btnEx4.TabIndex = 5;
+            this.btnEx4.Text = "Exercício 4";
+            this.btnEx4.UseVisualStyleBackColor = true;
+            this.btnEx4.Click += new System.EventHandler(this.BtnEx4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEx4);
+            this.Controls.Add(this.btnEx6);
+            this.Controls.Add(this.btnEx5);
+            this.Controls.Add(this.btnEx3);
+            this.Controls.Add(this.btnEx2);
+            this.Controls.Add(this.btnEx1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEx1;
+        private System.Windows.Forms.Button btnEx2;
+        private System.Windows.Forms.Button btnEx3;
+        private System.Windows.Forms.Button btnEx5;
+        private System.Windows.Forms.Button btnEx6;
+        private System.Windows.Forms.Button btnEx4;
+    }
+}
+
