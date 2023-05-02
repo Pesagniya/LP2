e999fe978e984bf11d5778f271c15c8c842c3dd2
Guilherme U. Iwasaki <101954273+Pesagniya@users.noreply.github.com>
2023-05-02 11:16:55 -0300

    Add files via upload

---
+++ PLacos/PLacos/frmExercicio3.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace PLacos
+{
+    partial class frmExercicio3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChecar = new System.Windows.Forms.Button();
+            this.txtPalindromo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnChecar
+            // 
+            this.btnChecar.Location = new System.Drawing.Point(194, 157);
+            this.btnChecar.Name = "btnChecar";
+            this.btnChecar.Size = new System.Drawing.Size(118, 91);
+            this.btnChecar.TabIndex = 0;
+            this.btnChecar.Text = "A palavra inserida é um palíndromo?";
+            this.btnChecar.UseVisualStyleBackColor = true;
+            this.btnChecar.Click += new System.EventHandler(this.BtnChecar_Click);
+            // 
+            // txtPalindromo
+            // 
+            this.txtPalindromo.Location = new System.Drawing.Point(354, 193);
+            this.txtPalindromo.Name = "txtPalindromo";
+            this.txtPalindromo.Size = new System.Drawing.Size(311, 20);
+            this.txtPalindromo.TabIndex = 1;
+            // 
+            // frmExercicio3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPalindromo);
+            this.Controls.Add(this.btnChecar);
+            this.Name = "frmExercicio3";
+            this.Text = "frmExercicio3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChecar;
+        private System.Windows.Forms.TextBox txtPalindromo;
+    }
+}
---
+++ PLacos/PLacos/frmExercicio3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PLacos
+{
+    public partial class frmExercicio3 : Form
+    {
+        public frmExercicio3()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnChecar_Click(object sender, EventArgs e)
+        {
+            string texto = (txtPalindromo.Text.Replace(" ", "")).ToLower();
+
+            if (txtPalindromo.Text.Length > 50)
+                MessageBox.Show("Palavra inválida, máximo de 50 caracteres.");
+            else
+                if (texto.ToArray())
+                
+        }
+    }
+}
