98ba21b53c52a4381677109ae14bbf0f4ba0ee60
Guilherme U. Iwasaki <101954273+Pesagniya@users.noreply.github.com>
2023-05-02 09:16:19 -0300

    Add files via upload

---
+++ PFuncoes/PFuncoes/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace PFuncoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSoma1 = new System.Windows.Forms.Button();
+            this.btnSoma2 = new System.Windows.Forms.Button();
+            this.btnSoma3 = new System.Windows.Forms.Button();
+            this.btnSoma4 = new System.Windows.Forms.Button();
+            this.btnSoma5 = new System.Windows.Forms.Button();
+            this.btnSoma6 = new System.Windows.Forms.Button();
+            this.btnSoma7 = new System.Windows.Forms.Button();
+            this.btnSoma8 = new System.Windows.Forms.Button();
+            this.btnSoma9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSoma1
+            // 
+            this.btnSoma1.Location = new System.Drawing.Point(182, 87);
+            this.btnSoma1.Name = "btnSoma1";
+            this.btnSoma1.Size = new System.Drawing.Size(115, 71);
+            this.btnSoma1.TabIndex = 0;
+            this.btnSoma1.Text = "Função Void";
+            this.btnSoma1.UseVisualStyleBackColor = true;
+            this.btnSoma1.Click += new System.EventHandler(this.BtnSoma1_Click);
+            // 
+            // btnSoma2
+            // 
+            this.btnSoma2.Location = new System.Drawing.Point(182, 177);
+            this.btnSoma2.Name = "btnSoma2";
+            this.btnSoma2.Size = new System.Drawing.Size(115, 71);
+            this.btnSoma2.TabIndex = 1;
+            this.btnSoma2.Text = "Função com resultado";
+            this.btnSoma2.UseVisualStyleBackColor = true;
+            this.btnSoma2.Click += new System.EventHandler(this.BtnSoma2_Click);
+            // 
+            // btnSoma3
+            // 
+            this.btnSoma3.Location = new System.Drawing.Point(182, 266);
+            this.btnSoma3.Name = "btnSoma3";
+            this.btnSoma3.Size = new System.Drawing.Size(115, 71);
+            this.btnSoma3.TabIndex = 2;
+            this.btnSoma3.Text = "Função Void com parâmetros";
+            this.btnSoma3.UseVisualStyleBackColor = true;
+            this.btnSoma3.Click += new System.EventHandler(this.BtnSoma3_Click);
+            // 
+            // btnSoma4
+            // 
+            this.btnSoma4.Location = new System.Drawing.Point(339, 87);
+            this.btnSoma4.Name = "btnSoma4";
+            this.btnSoma4.Size = new System.Drawing.Size(115, 71);
+            this.btnSoma4.TabIndex = 3;
+            this.btnSoma4.Text = "Função com Retorno e parâmetros";
+            this.btnSoma4.UseVisualStyleBackColor = true;
+            this.btnSoma4.Click += new System.EventHandler(this.BtnSoma4_Click);
+            // 
+            // btnSoma5
+            // 
+            this.btnSoma5.Location = new System.Drawing.Point(339, 177);
+            this.btnSoma5.Name = "btnSoma5";
+            this.btnSoma5.Size = new System.Drawing.Size(115, 71);
+            this.btnSoma5.TabIndex = 4;
+            this.btnSoma5.Text = "Função Void com parâmetros por referência";
+            this.btnSoma5.UseVisualStyleBackColor = true;
+            this.btnSoma5.Click += new System.EventHandler(this.BtnSoma5_Click);
+            // 
+            // btnSoma6
+            // 
+            this.btnSoma6.Location = new System.Drawing.Point(339, 266);
+            this.btnSoma6.Name = "btnSoma6";
+            this.btnSoma6.Size = new System.Drawing.Size(115, 71);
+            this.btnSoma6.TabIndex = 5;
+            this.btnSoma6.Text = "Função Void com Retorno e com parâmetros por referência";
+            this.btnSoma6.UseVisualStyleBackColor = true;
+            this.btnSoma6.Click += new System.EventHandler(this.BtnSoma6_Click);
+            // 
+            // btnSoma7
+            // 
+            this.btnSoma7.Location = new System.Drawing.Point(490, 87);
+            this.btnSoma7.Name = "btnSoma7";
+            this.btnSoma7.Size = new System.Drawing.Size(115, 71);
+            this.btnSoma7.TabIndex = 6;
+            this.btnSoma7.Text = "Função Void com parâmetro de saída";
+            this.btnSoma7.UseVisualStyleBackColor = true;
+            this.btnSoma7.Click += new System.EventHandler(this.BtnSoma7_Click);
+            // 
+            // btnSoma8
+            // 
+            this.btnSoma8.Location = new System.Drawing.Point(490, 177);
+            this.btnSoma8.Name = "btnSoma8";
+            this.btnSoma8.Size = new System.Drawing.Size(115, 71);
+            this.btnSoma8.TabIndex = 7;
+            this.btnSoma8.Text = "Função Void com retorno e com parâmetro de saída";
+            this.btnSoma8.UseVisualStyleBackColor = true;
+            this.btnSoma8.Click += new System.EventHandler(this.BtnSoma8_Click);
+            // 
+            // btnSoma9
+            // 
+            this.btnSoma9.Location = new System.Drawing.Point(490, 266);
+            this.btnSoma9.Name = "btnSoma9";
+            this.btnSoma9.Size = new System.Drawing.Size(115, 71);
+            this.btnSoma9.TabIndex = 8;
+            this.btnSoma9.Text = "Função Void com parâmetro de saída e com parâmetro opcional";
+            this.btnSoma9.UseVisualStyleBackColor = true;
+            this.btnSoma9.Click += new System.EventHandler(this.BtnSoma9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSoma9);
+            this.Controls.Add(this.btnSoma8);
+            this.Controls.Add(this.btnSoma7);
+            this.Controls.Add(this.btnSoma6);
+            this.Controls.Add(this.btnSoma5);
+            this.Controls.Add(this.btnSoma4);
+            this.Controls.Add(this.btnSoma3);
+            this.Controls.Add(this.btnSoma2);
+            this.Controls.Add(this.btnSoma1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSoma1;
+        private System.Windows.Forms.Button btnSoma2;
+        private System.Windows.Forms.Button btnSoma3;
+        private System.Windows.Forms.Button btnSoma4;
+        private System.Windows.Forms.Button btnSoma5;
+        private System.Windows.Forms.Button btnSoma6;
+        private System.Windows.Forms.Button btnSoma7;
+        private System.Windows.Forms.Button btnSoma8;
+        private System.Windows.Forms.Button btnSoma9;
+    }
+}
+
---
+++ PFuncoes/PFuncoes/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PFuncoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Soma1()
+        {
+            double resultado = 2 + 1;
+            MessageBox.Show(resultado.ToString());
+        }
+
+        private void BtnSoma1_Click(object sender, EventArgs e)
+        {
+            Soma1();
+        }
+
+        double Soma2()
+        {
+            double resultado = 2 + 3;
+            return resultado;
+        }
+
+        private void BtnSoma2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Soma2().ToString());
+        }
+
+        void Soma3(double x, double y)
+        {
+            x = 40;
+            double resultado = x + y;
+            MessageBox.Show("O resultado é: " + resultado.ToString());
+        }
+
+        private void BtnSoma3_Click(object sender, EventArgs e)
+        {
+            double a = 10;
+            double b = 20;
+
+            Soma3(a, b);
+            MessageBox.Show("Valor de a: " + a + "\nValor de b: " + b);
+        }
+
+        double Soma4(double x, double y)
+        {
+            double resultado = x + y;
+            return resultado;
+        }
+
+        private void BtnSoma4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Soma4(10, 20).ToString());
+        }
+
+        void Soma5(ref double resultado, ref double a)
+        {
+            resultado = 2 + 3;
+            a = 40;
+        }
+
+        private void BtnSoma5_Click(object sender, EventArgs e)
+        {
+            double a = 10, b = 20;
+            Soma5(ref a, ref b);
+            MessageBox.Show("Valor de a: " + a + "\nValor de b: " + b);
+        }
+
+        double Soma6(ref double resultado)
+        {
+            resultado = 2 + 3;
+            return resultado;
+        }
+
+        private void BtnSoma6_Click(object sender, EventArgs e)
+        {
+            double r = 0;
+            MessageBox.Show(Soma6(ref r).ToString());
+            MessageBox.Show(r.ToString());
+        }
+
+        void Soma7(out double resultado)
+        {
+            resultado = 2 + 3;
+        }
+
+        private void BtnSoma7_Click(object sender, EventArgs e)
+        {
+            double a = 50;
+            Soma7(out a);
+            MessageBox.Show(a.ToString());
+        }
+
+        double Soma8(out double resultado)
+        {
+            resultado = 2 + 3;
+            return resultado;
+        }
+
+        private void BtnSoma8_Click(object sender, EventArgs e)
+        {
+            double a = 50;
+            MessageBox.Show(Soma8(out a).ToString());
+            MessageBox.Show(a.ToString());
+        }
+
+        void Soma9(out double retorno, double x, double y, double z = 0)
+        {
+            if (z > 0)
+                retorno = (x + y + z) / 3;
+            else
+                retorno = (x + y) / 2;
+        }
+
+        private void BtnSoma9_Click(object sender, EventArgs e)
+        {
+            double r = 0;
+            Soma9(out r, 10, 20, 30);
+            MessageBox.Show(r.ToString());
+        }
+    }
+}
